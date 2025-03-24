3cdf1d74f3c30ff028d6843c51de347b824a5c0a
unknown <natnaelm552@gmail.com>
2025-03-24 12:09:20 +0300

    amount input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,14 +8,41 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
-            decoration: InputDecoration(label: Text("Title")),
+            controller: _titleController,
+            decoration: const InputDecoration(label: Text("Title")),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(prefixText: '\$', label: Text("Amount")),
+            controller: _amountController,
+          ),
+          Row(
+            children: [
+              TextButton(onPressed: () {}, child: const Text("Cancel")),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text("Save Expense")),
+            ],
           )
         ],
       ),
